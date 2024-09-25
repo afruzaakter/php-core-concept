e6033dbd03b3fa3d9a04bd7ae7c1968c1076c57f
afruzaakter <afruzaaktersonia1995@gmail.com>
2024-09-25 16:18:19 +0600

    add

---
+++ main.php
@@ -0,0 +1,18 @@
+
+<?php 
+
+// require"./header.php";
+
+include "./header.php";
+echo " <h1>main file</h1>";
+include "./footer.php";
+
+?>
+
+
+// include and require ai 2 process a file niye asa jai 
+
+// include korle kono file a proble hole code run kore but require korle oporer kono file a problem hole code are run kore na.
+
+// normal file er jonno include korbo and function er jonno require use korte hobe 
+
